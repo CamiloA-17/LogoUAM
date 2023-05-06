f356200695fc00158d56d1f3ae2ce7ae5b111c8d
Camilo <camiangi1517@gmail.com>
2023-05-06 18:11:00 -0500

    Adding project bases

---
+++ Logo/src/co/autonoma/edu/game/elements/Turtle.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.autonoma.edu.game.elements;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Turtle {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private double angle;
+    private Color pencilColor;
+
+    public Turtle(int x, int y, int width, int height, double angle, Color pencilColor) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.angle = angle;
+        this.pencilColor = pencilColor;
+    }
+    
+    public void backward(int distance){
+        
+    }
+    
+    public void forward(int distance){
+        
+    }
+    
+    public void rightTurn(double angle){
+        
+    }
+    
+    public void leftTurn(double angle){
+        
+    }
+    
+    public void home(){
+        
+    }
+    
+    public void repeat(int cant, ArrayList<String> instruccionts){
+        
+    }
+}
