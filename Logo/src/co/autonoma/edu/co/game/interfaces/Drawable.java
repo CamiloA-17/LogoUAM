66558f99ac4bcc03321d8b001431ae155968972d
MartinOstios <martin.ostios.arias@gmail.com>
2023-05-07 20:25:13 -0500

    Test archivos

---
+++ Logo/src/co/autonoma/edu/co/game/interfaces/Drawable.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package co.autonoma.edu.co.game.interfaces;
+
+import javax.swing.DefaultListModel;
+
+/**
+ *
+ * @author izibr
+ */
+public interface Drawable {
+    public void redraw();
+    public void fillList(DefaultListModel data);
+}
--- Logo/src/co/autonoma/edu/game/controller/Controller.java
+++ Logo/src/co/autonoma/edu/game/controller/Controller.java
@@ -3,8 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package co.autonoma.edu.game.controller;
-
-import co.autonoma.edu.game.files.DocumentReader;
 import co.autonoma.edu.game.files.InstructionsManager;
 import co.autonoma.edu.game.gui.Window;
 
@@ -16,9 +14,9 @@ public class Controller {
     public static void main(String[] args) {
         Window mainWindow = new Window();
         InstructionsManager manager = new InstructionsManager();
-        DocumentReader reader = new DocumentReader();
-        String file= reader.searchFile();
-        System.out.println(reader.readFile());
+        //DocumentReader reader = new DocumentReader();
+        //String file= reader.searchFile();
+        //System.out.println(reader.readFile());
         mainWindow.setManager(manager);
         mainWindow.setVisible(true);
     }
